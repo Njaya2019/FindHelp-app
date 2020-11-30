a7bf9e004f87c1b2235aeb6739e277c88e5e1b41
Andrew <njayaandrew@gmail.com>
2020-11-30 13:43:19 +0300

    [chore] changed databse enviroment variable

--- configurations.py
+++ configurations.py
@@ -6,7 +6,7 @@ class Config(object):
     TESTING = False
     UPLOAD_FOLDER = "app\\image_uploads"
     # MAX_CONTENT_LENGTH = 
-    DATABASE_URI = os.getenv('DB_URL')
+    DATABASE_URI = os.getenv('DATABASE_URL')
     # DATABASE_URI = os.environ['DB_URL']
 
 class ProductionConfig(Config):
